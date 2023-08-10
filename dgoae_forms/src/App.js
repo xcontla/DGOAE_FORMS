c9fb2e3277f7354267da96480df1c7da1ad86345
angelmosiah.velay <angelmosiah.velay@bbva.com>
2023-08-10 17:03:44 -0600

    Cambio en App.js , puerto 3001 y home:forms

--- dgoae_forms/src/App.js
+++ dgoae_forms/src/App.js
@@ -10,37 +10,61 @@ import Submitted from "./components/Submitted";
 import SaveForm from "./components/SaveForm";
 import Credits from "./pages/Credits";
 import Contact from "./pages/Contact";
+import Test from "./pages/Test";
 
 function App() {
-  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
-      {isAuthenticated ? (
+   
         <>
           <Router>
             <Routes>
-              <Route path="/" element={<Welcome />} />
-              <Route path="/form/:id" element={<Forms />} />
-              <Route path="/viewresponse/:id" element={<ViewUserForm />} />
-              <Route path="/saveform/:id" element={<SaveForm />} />
-              <Route path="/credits" element={<Credits />} />
-              <Route path="/contact" element={<Contact />} />
+              <Route path="/" element={<Test />} />
+              
             </Routes>
           </Router>
         </>
-      ) : (
-        <>
-          <Router>
-            <Routes>
-              <Route path="/" element={<LoginButton />} />
-              <Route path="/response/:global_id" element={<UserForm />} />
-              <Route path="/submitted/:global_id" element={<Submitted />} />
-            </Routes>
-          </Router>
-        </>
-      )}
+
+     
     </div>
   );
 }
 
 export default App;
+
+
+// function App() {
+//   const { isAuthenticated } = useAuth0();
+//   return (
+//     <div className="App">
+//       {isAuthenticated ? (
+//         <>
+//           <Router>
+//             <Routes>
+//               <Route path="/" element={<Welcome />} />
+//               <Route path="/form/:id" element={<Forms />} />
+//               <Route path="/viewresponse/:id" element={<ViewUserForm />} />
+//               <Route path="/saveform/:id" element={<SaveForm />} />
+//               <Route path="/credits" element={<Credits />} />
+//               <Route path="/contact" element={<Contact />} />
+//             </Routes>
+//           </Router>
+//         </>
+//       ) : (
+//         <>
+//           <Router>
+//             <Routes>
+//               <Route path="/" element={<LoginButton />} />
+//               <Route path="/response/:global_id" element={<UserForm />} />
+//               <Route path="/submitted/:global_id" element={<Submitted />} />
+//             </Routes>
+//           </Router>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
--- dgoae_forms/src/index.js
+++ dgoae_forms/src/index.js
@@ -6,15 +6,16 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './components/StateProvider'
 import Reducer, { initialState } from './components/Reducer'; { }
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider domain='dev-w5pbf864.us.auth0.com' clientId='G1UE0TbeEBLNKDVQ02wF04jL7ybBdCZO' redirectUri={window.location.origin}>
+    
       <StateProvider initialState={initialState} Reducer={Reducer}>
  
         <App />
       
       </StateProvider>
-    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -23,3 +24,22 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Auth0Provider domain='dev-w5pbf864.us.auth0.com' clientId='G1UE0TbeEBLNKDVQ02wF04jL7ybBdCZO' redirectUri={window.location.origin}>
+//       <StateProvider initialState={initialState} Reducer={Reducer}>
+ 
+//         <App />
+      
+//       </StateProvider>
+//     </Auth0Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
