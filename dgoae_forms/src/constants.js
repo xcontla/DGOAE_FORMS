d315f24b22b4c150337ada7818eab0a4d3a20fe6
Xavier Contla <xcontla@comunidad.unam.mx>
2023-08-18 13:39:27 -0600

    Actualización en frontend, imagenes, creditos, contacto

---
+++ dgoae_forms/src/constants.js
@@ -0,0 +1,13 @@
+
+
+export const MAIN_URL = "";
+export const APP_URL = "http://localhost:3001/dgoaeforms";
+export const API_URL = "http://localhost:9000";
+//export const DEFAULT_COLOR = "#333";
+
+//export const MAIN_URL = "dgoae.digitaloe.unam.mx";
+export const APP_URL2 = "/dgoaeforms";
+//export const API_URL = "/apiforms";
+
+
+export const MAX_OPCIONES = 10;
